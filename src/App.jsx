85affd00a09bfbb07d11a11285c902407f0b2b9e
Roberto Núñez <Roberto>
2023-05-24 21:33:08 -0500

    Project has been finished

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,17 @@ import TaskForm from "./components/TaskForm"
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" exact element={<TasksList />} />
-        <Route path="/create" exact element={<TaskForm />} />
-        <Route path="/edit/:id" exact element={<TaskForm />} />
-      </Routes>
-    </Router>
+    <div className="bg-zinc-900 h-screen text-white">
+      <div className="flex items-center justify-center h-full">
+        <Router>
+          <Routes>
+            <Route path="/" exact element={<TasksList />} />
+            <Route path="/create" exact element={<TaskForm />} />
+            <Route path="/edit/:id" exact element={<TaskForm />} />
+          </Routes>
+        </Router>
+      </div>
+    </div>
   )
 }
 
